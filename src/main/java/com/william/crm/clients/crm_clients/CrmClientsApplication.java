30a213eb52964660950e22f498d7883a4ce17f88
William Wun <williamwun@gmail.com>
2024-08-09 14:53:21 -0700

    project started

---
+++ src/main/java/com/william/crm/clients/crm_clients/CrmClientsApplication.java
@@ -0,0 +1,13 @@
+package com.william.crm.clients.crm_clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/crm/clients/crm_clients/CrmClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.crm.clients.crm_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
